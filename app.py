ae33731660427db2f37129ad722eef56422374b0
jhammarstedt <johan.hammarstedt@gmail.com>
2020-11-14 00:29:34 +0100

    added hello and goodbye message

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ app = Flask(__name__)
 ACCESS_TOKEN =data['tokens'][0]['Access']
 VERIFY_TOKEN = data['tokens'][0]['Verify']
 bot = Bot(ACCESS_TOKEN)
+COUNT = 0 #message count
 
 @app.route('/',methods= ['GET','POST'])
 def receive_messeage():
+    global COUNT
     if request.method == 'GET':
         # confirms that all requests that your bot receives came from fb
         token_sent = request.args.get("hub.verify_token")
         return verify_fb_token(token_sent)
     else:  # if the request was not get, it will be a POST - fb is sending the bot a message sent by the user.
-
         # Get our message that the user sent the bot
         output = request.get_json()
         for event in output['entry']:
@@ -29,8 +30,21 @@ def receive_messeage():
                     # Fb messenger ID for user so we know where to send reponse back to
                     recipient_id = message['sender']['id']
                     if message['message'].get('text'):  # if there is text here
-                        response_sent_text = get_message()
-                        send_message(recipient_id, response_sent_text)
+                        #Here we get the message and will handle how we respond:
+                        user_message = message['message'].get('text')
+
+                        print(user_message)
+                        if COUNT == 0: #quick fix for initial message
+                            initial_response = 'Hi, I am  the Movie Bot! What type of movies do you seek today?'
+                            send_message(recipient_id,initial_response)
+                            COUNT += 1
+                        elif user_message in ['bye','Bye','Goodbye']:
+                            goodbye = 'Hope you enjoy the movie, bye!'
+                            send_message(recipient_id,goodbye)
+                        else:
+
+                            response_sent_text = get_message() #now we just send back a random message
+                            send_message(recipient_id, response_sent_text)
                     # if users sends something else than text (gif, photo etc..)
                     if message['message'].get('attachments'):
                         response_sent_notext = get_message()
@@ -45,6 +59,7 @@ def send_message(recipient_id,response):
 
 def get_message():
     samples = ['Great Job!','Keep being cool!','This actually works!']
+    samples = ['I only recommend the best!','I swear you will be happy with me!', 'My role here is to serve you!']
     return random.choice(samples)
 
 def verify_fb_token(token_sent):
